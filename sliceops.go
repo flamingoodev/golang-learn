67120f92607df929df66c78224d149f06149680f
FLAMINGO <flamingodev@outlook.com>
2020-05-25 00:53:40 +0800

    update sliceops

--- sliceops.go
+++ sliceops.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func printSlice(s []int) {
-	fmt.Printf("len=%d, cap=%d\n", len(s), cap())
+	fmt.Printf("slice=%d, len=%d, cap=%d\n", s, len(s), cap(s))
 }
 func main() {
 	// 定义slice
@@ -13,4 +13,31 @@ func main() {
 		s = append(s, 2*i+1)
 	}
 	fmt.Println(s)
+	// 创建slice
+	s5 := []int{2, 4, 6, 8}
+	printSlice(s5)
+	s6 := make([]int, 10)
+	printSlice(s6)
+	s7 := make([]int, 10, 30)
+	printSlice(s7)
+	// copy
+	fmt.Println("Copying slice")
+	copy(s6, s7)
+	printSlice(s6)
+	// delete
+	fmt.Println("Deleting elements form slice")
+	s7 = append(s7[:3], s6[4:]...)
+	printSlice(s7)
+	// pop
+	fmt.Println("Popping from front")
+	front := s7[0]
+	s7 = s7[1:]
+	fmt.Printf("pop %d\n", front)
+	printSlice(s7)
+
+	fmt.Println("Popping from back")
+	back := s7[len(s7)-1]
+	s7 = s7[:len(s7)-1]
+	fmt.Printf("pop %d\n", back)
+	printSlice(s7)
 }
